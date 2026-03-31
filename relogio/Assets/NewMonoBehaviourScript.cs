c9a0e5f6b4a542cffe0b1c80a3560b7e073f6ed1
eduardoandrade10879 <eduardoandradesenai@gmail.com>
2026-03-31 11:23:33 -0300

    relogio segundos

---
+++ relogio/Assets/NewMonoBehaviourScript.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using System;
+
+public class relogio : MonoBehaviour
+
+{
+    [SerializeField] int segundos_atuais;
+    [SerializeField] Transform ponteiro_segundo;
+    Vector3 segundo_rotacao = Vector3.zero;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+        segundos_atuais = DateTime.Now.Second;
+        segundo_rotacao = ponteiro_segundo.localEulerAngles;
+        segundo_rotacao.y = segundos_atuais * 6 * (-1);
+        ponteiro_segundo.localEulerAngles = segundo_rotacao;
+    }
+}
+
+
+
+
